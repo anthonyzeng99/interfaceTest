c7d52c4b674c1b5878ae188a17ab598b3532c4e4
anthonyzeng99 <anthonyzeng99@gmail.com>
2015-12-13 21:37:49 -0500

    Added getVolume() for Sphere

--- Sphere.java
+++ Sphere.java
@@ -5,6 +5,10 @@ public class Sphere extends Circle{
 	super(radius,name);
     }
 
+    public double getVolume() {
+	return (4/3) * Math.PI * Math.pow(radius,3);
+    }
+
     public String toString(){
 	return "FIXME!";
     }
